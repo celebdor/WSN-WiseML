1fcca24b179dc8269c920c56347b14aa2b87c398
Antoni S. Puimedon <celebdor@gmail.com>
2011-03-02 13:01:31 +0100

    Added recovery of all the data set and finalized passwd params

--- extractor.py
+++ extractor.py
@@ -14,13 +14,14 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with WSN-visor.  If not, see <http://www.gnu.org/licenses/>.
 """
-import urllib2
-import urllib
+import urllib2, sys, urllib
 from cookielib import CookieJar
 
 url = 'http://meteoroleg.upc.es/dexserver/j_spring_security_check'
-urlRep = 'http://meteoroleg.upc.es/dexserver/report-results.htm?6578706f7274=1&d-49653-e=1&queryId=83'
-login = { 'j_username': , 'j_password':  }
+urlTemp = 'http://meteoroleg.upc.es/dexserver/report-results.htm?6578706f7274=1&d-49653-e=1&queryId=83'
+urlHum = 'http://meteoroleg.upc.es/dexserver/report-results.htm?6578706f7274=1&d-49653-e=1&queryId=84'
+urlLight = 'http://meteoroleg.upc.es/dexserver/report-results.htm?6578706f7274=1&d-49653-e=1&queryId=85'
+login = { 'j_username': sys.argv[1] , 'j_password': sys.argv[2] }
 headers = {'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US)'}
 loginFormData = urllib.urlencode(login)
 
@@ -33,11 +34,26 @@ cookies.extract_cookies(resp, req)
 cookie_handler = urllib2.HTTPCookieProcessor(cookies)
 redirect_handler = urllib2.HTTPRedirectHandler()
 opener = urllib2.build_opener(redirect_handler, cookie_handler)
+#Making the initial connection for the login
 opener.open(req)
-req2 =  urllib2.Request(urlRep, dict(), headers)
-resp2 = opener.open(req2)
+reqTemp =  urllib2.Request(urlTemp, dict(), headers)
+reqHum =  urllib2.Request(urlHum, dict(), headers)
+reqLight =  urllib2.Request(urlLight, dict(), headers)
+respTemp = opener.open(reqTemp)
+respHum = opener.open(reqHum)
+respLight = opener.open(reqLight)
 
-print len(cookies)
+ft = open('y_temp.csv','w')
+fh = open('y_hum.csv','w')
+fl = open('y_lum.csv','w')
 
-for l in resp2:
-     print l
+for l in respTemp:
+     ft.write(l)
+for l in respHum:
+     fh.write(l)
+for l in respLight:
+     fl.write(l)
+
+ft.close()
+fh.close()
+fl.close()
