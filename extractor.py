1744f9d2180a7d19288e11cce30ce06c170249e4
Antoni Segura Puimedon <antoni@antoni-HP-Compaq-6910p.(none)>
2011-03-05 21:46:34 +0100

    Added wiseml output, pending positions and scenario description

--- extractor.py
+++ extractor.py
@@ -16,10 +16,10 @@ along with WSN-visor.  If not, see <http://www.gnu.org/licenses/>.
 """
 import urllib2, sys, urllib, re
 from cookielib import CookieJar
-#from dateutil.parser import parse
-from xml import etree
+from lxml import etree
 import pickle
-from datetime import datetime as dt
+import datetime
+from operator import attrgetter
 
 class trace:
      times = dict()
@@ -33,7 +33,7 @@ class trace:
           try:
                return trace.times[tup]
           except KeyError:
-               t = dt(*tup)
+               t = datetime.datetime(*tup)
                trace.times[tup] = t
                return t
 
@@ -81,6 +81,20 @@ class trace:
      def __str__(self):
           return '('+str(self.time)+':'+self.nodeId+'_'+self.name+')->'+str(self.temp)+'C '+str(self.hum)+'% '+str(self.lum)+'LUX'
 
+     def toXml(self):
+          e = etree.Element('node')
+          e.attrib['id'] = 'urn:wisebed:upc:'+self.nodeId
+          temp = etree.SubElement(e, 'data')
+          temp.attrib['key'] = 'urn:wisebed:upc:node:capability:temperature'
+          temp.text = str(self.temp)
+          hum = etree.SubElement(e, 'data')
+          hum.attrib['key'] = 'urn:wisebed:upc:node:capability:humidity'
+          hum.text = str(self.hum)
+          lum = etree.SubElement(e, 'data')
+          lum.attrib['key'] = 'urn:wisebed:upc:node:capability:luminosity'
+          lum.text = str(self.lum)
+          return e
+
 class experiment:
      def __init__(self):
           self.traces = dict()
@@ -110,6 +124,25 @@ class experiment:
      
      def __str__(self):
           return '\n'.join([str(t) for t in self.traces.values()])+'\n\n'
+
+     def toXml(self):
+          traceList =  self.traces.values()
+          traceList.sort(key=attrgetter('time'), reverse=False)
+          it = traceList[0].time
+          pd = datetime.timedelta(-1)
+          root = etree.Element('wiseml')
+          root.attrib['version'] = '1.0'
+          root.attrib['xmlns'] = 'http://wisebed.eu/ns/wiseml/1.0'
+          for e in  traceList:
+               t = e.time-it
+               if pd != t:
+                    ts = etree.Element('timestamp')
+                    ts.text = str(t.seconds)
+                    root.append(ts)
+                    pd = t
+               root.append(e.toXml())
+          return root
+          
      
 def traceProcess(lhs, rhs):
      return experiment(lhs)+trace(rhs)
@@ -152,5 +185,4 @@ else:
 
 
 trRed = reduce(traceProcess, l)
-
-print trRed
+print etree.tostring(trRed.toXml(), pretty_print = True)
